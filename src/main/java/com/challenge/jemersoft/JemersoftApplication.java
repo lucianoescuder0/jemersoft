def45d5e997abd1afe9986c98956e20cbe82448d
lescudero <lescudero@runaid.com.ar>
2024-02-24 12:31:08 -0300

    Base del chall

---
+++ src/main/java/com/challenge/jemersoft/JemersoftApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.jemersoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JemersoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JemersoftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenge/jemersoft/JemersoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.jemersoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JemersoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
